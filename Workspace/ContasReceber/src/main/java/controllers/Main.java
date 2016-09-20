be7d7306fb17069e696c3a0aeb6a27b77b09ded7
Eduardo Corrêa de Sá <eduardo.correa.sa@gmail.com>
2016-09-19 21:58:08 -0300

    Subindo projeto ProgWeb

---
+++ Workspace/ContasReceber/src/main/java/controllers/Main.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import views.Home;
+
+public class Main {
+	
+	public static void main( String[] args ){
+
+		Home principal = new Home();
+	       principal.setVisible(true);
+
+
+	}
+}
